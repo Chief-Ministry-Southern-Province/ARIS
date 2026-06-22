d9ff7506313beedd274b5e57ac3dabae80bad054
ThilinaRodrigo <thilinalakshan2001@gmail.com>
2026-06-22 21:05:35 +0530

    Add Institution model and migration for institutions table

---
+++ aris-backend/app/Models/Institution.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Institution extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'type',
+        'address',
+        'contact_number',
+        'district',
+        'province',
+        'head_of_institution',
+        'parent_institution_id',
+        'direct_to_rdhs'
+    ];
+
+    public function parentInstitution()
+    {
+        return $this->belongsTo(Institution::class, 'parent_institution_id');
+    }
+
+    public function childInstitutions()
+    {
+        return $this->hasMany(Institution::class, 'parent_institution_id');
+    }
+
+}
